1f504e7bafbc025672e1fbf71a1346ee3fee94b3
Osman <58899378+JGUsman007@users.noreply.github.com>
2025-01-23 18:48:34 +0500

    hotbar fix

--- web/src/components/inventory/InventoryHotbar.tsx
+++ web/src/components/inventory/InventoryHotbar.tsx
@@ -31,35 +31,23 @@ const InventoryHotbar: React.FC = () => {
           <div
             className="hotbar-item-slot"
             style={{
+                      backgroundColor: isSlotWithItem(item) ? 'rgba(142, 142, 142,0.63)' : 'rgba(71, 71, 71, 0.63)',
               backgroundImage: `url(${item?.name ? getItemUrl(item as SlotWithItem) : 'none'}`,
             }}
             key={`hotbar-${item.slot}`}
           >
             {isSlotWithItem(item) && (
               <div className="item-slot-wrapper">
-                <div className="hotbar-slot-header-wrapper">
+                <div className="item-slot-header-wrapper">
                   <div className="inventory-slot-number">{item.slot}</div>
-                  <div className="item-slot-info-wrapper">
-                    <p>
-                      {item.weight > 0
-                        ? item.weight >= 1000
-                          ? `${(item.weight / 1000).toLocaleString('en-us', {
-                              minimumFractionDigits: 2,
-                            })}kg `
-                          : `${item.weight.toLocaleString('en-us', {
-                              minimumFractionDigits: 0,
-                            })}g `
-                        : ''}
-                    </p>
-                    <p>{item.count ? item.count.toLocaleString('en-us') + `x` : ''}</p>
-                  </div>
+                  <p>{item.count ? item.count.toLocaleString('en-us') + `x` : ''}</p>
                 </div>
                 <div>
-                  {item?.durability !== undefined && <WeightBar percent={item.durability} durability />}
                   <div className="inventory-slot-label-box">
                     <div className="inventory-slot-label-text">
                       {item.metadata?.label ? item.metadata.label : Items[item.name]?.label || item.name}
                     </div>
+                    {item?.durability !== undefined && <WeightBar percent={item.durability} durability />}
                   </div>
                 </div>
               </div>
